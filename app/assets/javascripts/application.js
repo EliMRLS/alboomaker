92bce25acbb0f223adf2e71e018979a264a4f1d3
EliMRLS <e.morales.cinta@gmail.com>
2018-09-13 16:58:18 +0200

    lkdh

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= xxxrrequire turbolinks
+//= xxxrequire turbolinks
 //= require jquery.stellar.min
 //= require jquery.waypoints.min
 //= require owl.carousel.min
