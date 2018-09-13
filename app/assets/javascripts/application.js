e69f5f53049151d3a8f489d1bfb63b3fa44c0dcb
EliMRLS <e.morales.cinta@gmail.com>
2018-09-13 18:28:48 +0200

    fix(home): add pics

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= xxxrequire turbolinks
+//= xxxrrequire turbolinks
 //= require jquery.stellar.min
 //= require jquery.waypoints.min
 //= require owl.carousel.min
