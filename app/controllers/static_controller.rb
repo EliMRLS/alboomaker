7865692a808d468ffdcca79c25ba34f481b6eb1d
EliMRLS <e.morales.cinta@gmail.com>
2018-09-13 12:05:28 +0200

    TPX multiline cart working

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,4 +2,7 @@ class StaticController < ApplicationController
   def home
     @products = Product.all
   end
+
+
+
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,13 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'tech@printoria.es'
   layout 'mailer'
+
+  def send_email(email_params)
+    @message = email_params[:message]
+    @sender_name = email_params[:sender_name]
+    mail(to: default_to("tech@printoria.es"),
+         subject: "Atención al cliente",
+         reply_to: email_params[:sender_email])
+  end
+
 end
