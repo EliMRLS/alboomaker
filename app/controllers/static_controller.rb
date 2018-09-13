0015433e7de72ad527c508013adcf117190207ab
EliMRLS <e.morales.cinta@gmail.com>
2018-09-13 20:32:44 +0200

    feat(home): add FAQ

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,6 +3,9 @@ class StaticController < ApplicationController
     @products = Product.all
   end
 
+  def legal
+  end
+
   def send_email
     redirect_to(:root, alert: "Todos los campos son obligatorios") and return if params[:contact].map{ |k, v| v.blank? }.any?
     redirect_to(:root, alert: "El campo email no es correcto") and return unless params[:contact].values[2] = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'static#home'
 
   post 'send_email', to: 'static#send_email'
+  get 'legal', to: 'static#legal'
 end
